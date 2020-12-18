99bf44b43c6bf2adf7093bdec981e410e62faf3a
Amanda Simmons <carpe737diem@gmail.com>
2020-12-18 10:35:41 -0500

    added formatted result of part b from readme

--- iss.py
+++ iss.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-__author__ = 'Amanda Simmons'
+__author__ = 'Amanda Simmons, Peter M'
+# https://stackoverflow.com/questions/3682748/converting-unix-timestamp-string-to-readable-date
 
 import requests
-
+from datetime import datetime
 
 def main():
     response_obj = requests.get('http://api.open-notify.org/astros.json')
@@ -14,6 +15,13 @@ def main():
     for astronaut in astronauts:
         print(f'astronaut: {astronaut["name"]}, craft: {astronaut["craft"]}, total astronauts in space: {astronaut_total}')
 
+    response_obj = requests.get('http://api.open-notify.org/iss-now.json')
+    data = response_obj.json()
+    # print(data)
+    timestamp = int(data['timestamp'])
+    human_read_ts = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    coords = data['iss_position']
+    print(f'current geographic coordinates of space station: {coords}, timestamp: {human_read_ts}')
 
 if __name__ == '__main__':
     main()
