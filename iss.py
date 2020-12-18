e38760221a227a95bb044487805cf70517540ddb
Amanda Simmons <carpe737diem@gmail.com>
2020-12-18 10:19:21 -0500

    returning formatted data from part a of readme

--- iss.py
+++ iss.py
@@ -2,9 +2,17 @@
 
 __author__ = 'Amanda Simmons'
 
+import requests
+
 
 def main():
-    pass
+    response_obj = requests.get('http://api.open-notify.org/astros.json')
+    data = response_obj.json()
+    astronauts = data['people']
+    astronaut_total = data['number']
+    # print(astronauts)
+    for astronaut in astronauts:
+        print(f'astronaut: {astronaut["name"]}, craft: {astronaut["craft"]}, total astronauts in space: {astronaut_total}')
 
 
 if __name__ == '__main__':
